5c33b7c6642c561e8ada601d43ca6248da315b01
Mike Campbell <mike.campbell1967@gmail.com>
2021-02-24 15:52:22 -0500

    built out the notes function for individual users

--- page-my-notes.php
+++ page-my-notes.php
@@ -14,6 +14,15 @@ if (!is_user_logged_in()) {
     
 
   <div class="container container--narrow page-section">
+
+  <div class="create-note">
+        <h2 class="headline headline--medium">Create New Note</h2>
+        <input class="new-note-title" placeholder="Title">
+        <textarea class="new-note-body" placeholder="Your note here..."></textarea>
+        <span class="submit-note">Create Note</span>
+        <span class="note-limit-message">Note limit reached: delete an existing note to make room for a new one.</span>
+      </div>
+
     <ul class="min-list link-list" id="my-notes">
         <?php
             $userNotes = new WP_QUERY(array(
@@ -23,17 +32,15 @@ if (!is_user_logged_in()) {
             ));
 
             while($userNotes->have_posts()) {
-                $userNotes->the_post(); ?>
-                <li data-id="<?php the_id() ?>">
-                    <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
-                    <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"> Edit</i></span>
-                    <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"> Delete</i></span>
-                    <textarea readonly class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
-                    <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"> Save</i></span>
-
-                </li>
+              $userNotes->the_post(); ?>
+              <li data-id="<?php the_ID(); ?>">
+                <input readonly class="note-title-field" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); ?>">
+                <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
+                <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
+                <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
+                <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
+              </li>
             <?php }
-
         ?>
     </ul>
   </div>
